14e6971224e9a75fac45b0c2a3a823f876cad69f
Siddharth Singh <siddharth7.singh@gmail.com>
2015-02-23 14:21:11 +0530

    fixed bugs

--- backendapertiummbasic.py
+++ backendapertiummbasic.py
@@ -21,13 +21,22 @@ class MyForm(QtGui.QMainWindow):
 			b='http://127.0.0.1:2737/translate?langpair=en|eo&q=%s' % (self.ui.input.text())
 			response = urllib.urlopen(b)
 			html = response.read()
-			nestr = re.sub(r'[^a-zA-Z ]',r'',html)
-			i=2
+			nestr = re.sub(r'[^a-zA-Z{}?! ]',r'',html)
+			#print nestr
 			translatedString=''
-			while(nestr.split()[i]!='responseDetails'):
-			    #if(nestr.split()[i]!='responseData' or nestr.split()[i]!='translatedText'):
-			    translatedString=translatedString+nestr.split()[i]+' '
+			i=0
+			flag=0
+			opening_brak=0
+			while(opening_brak!=2):
+			    if(nestr[i]=='{'):
+			        opening_brak+=1
+			        #print nestr[i]
 			    i+=1
+			i=i+15
+			while(nestr[i]!='}'):
+			    translatedString=translatedString+nestr[i]
+			    i+=1
+			#print a
 			response.close()
 			self.ui.output.setText(translatedString)
 if __name__=='__main__':	
