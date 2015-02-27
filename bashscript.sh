1299bac728577246a010cc738fde377cb6c9c216
Siddharth Singh <siddharth7.singh@gmail.com>
2015-02-28 00:03:05 +0530

    Update bashscript.sh

--- bashscript.sh
+++ bashscript.sh
@@ -1,5 +1,10 @@
 #!/bin/sh
+procs=(`ps -fA | grep  ./servlet.py | grep /usr | awk '{print $2}'`)
+for pid in $procs; do
+	kill $pid
+done ;
 chmod +x script1.sh
 chmod +x script2.sh
 nohup bash script1.sh &
+sleep 2
 nohup bash script2.sh &
